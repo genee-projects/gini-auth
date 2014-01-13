1dab9e2485429f2db32fdccf8e8e27988f377795
Jia Huang <jia.huang@geneegroup.com>
2014-01-13 18:14:01 +0800

    first commit

---
+++ class/gini/auth/rpc.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Gini\Auth;
+
+class RPC implements \Gini\Auth\Driver {
+
+    private $_rpc;
+    private $_opt;
+
+    function __construct(array $opt){
+        $this->_rpc = new \Gini\RPC($opt['rpc.url']);
+        $this->_opt = $opt;
+    }
+    //验证令牌/密码
+    function verify($username, $password) {
+
+        $nusername = preg_replace('/%[^%]+$/', '', $username . '|' . $this->_opt['backend']);
+
+        try {
+            $key = $this->_rpc->auth->verify($nusername, $password);
+            if ($key) {
+                $_SESSION['#RPC_TOKEN_KEY'][$this->_backend][$username] = $key;
+                return true;
+            }
+        }
+        catch (\Gini\RPC\Exception $e) {
+        }
+
+        return false;
+    }
+    //设置令牌
+    function change_username($username, $new_username) {
+        //安全问题 禁用
+        return false;
+    }
+    //设置密码
+    function change_password($username, $password) {
+        //安全问题 禁用
+        return false;
+    }
+    //添加令牌/密码对
+    function add($username, $password) {
+        //安全问题 禁用
+        return false;
+    }
+    //删除令牌/密码对
+    function remove($username) {
+        //安全问题 禁用
+        return false;
+
+    }
+
+}
