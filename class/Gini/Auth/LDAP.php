ba7bd919d95fdd69f987d8da7483039a914d7aee
Jia Huang <jia.huang@geneegroup.com>
2015-01-17 14:06:22 +0000

    we should use "username" instead of "token"

--- class/Gini/Auth/LDAP.php
+++ class/Gini/Auth/LDAP.php
@@ -18,69 +18,69 @@ class LDAP implements \Gini\Auth\Driver
     public function __construct(array $opt)
     {
         $this->options = $opt;
-        $this->ldap = \Gini\LDAP::factory($opt['options']);
+        $this->ldap = \Gini\LDAP::factory($opt['ldap.options']);
     }
 
-    public function verify($token, $password)
+    public function verify($username, $password)
     {
-        $opt = & $this->options;
-        $filter = sprintf('(%s=%s)', $opt['token_attr'], $token);
-        $sr = $this->ldap->search($opt['token_base'], $filter);
+        $opt = $this->options;
+        $filter = sprintf('(%s=%s)', $opt['ldap.username_attr'], $username);
+        $sr = $this->ldap->search($opt['ldap.username_base'], $filter);
         $entries = $this->ldap->entries($sr);
         if (!$entries['count']) return FALSE;
-        $token_dn = $entries[0]['dn'];
+        $username_dn = $entries[0]['dn'];
 
-        return $this->ldap->bind($token_dn, $password);
+        return $this->ldap->bind($username_dn, $password);
     }
 
-    public function changePassword($token, $password)
+    public function changePassword($username, $password)
     {
-        $opt = & $this->options;
-        $filter = sprintf('(%s=%s)', $opt['token_attr'], $token);
-        $sr = $this->ldap->search($opt['token_base'], $filter);
+        $opt = $this->options;
+        $filter = sprintf('(%s=%s)', $opt['ldap.username_attr'], $username);
+        $sr = $this->ldap->search($opt['ldap.username_base'], $filter);
         $entries = $this->ldap->entries($sr);
         if (!$entries['count']) return FALSE;
-        $token_dn = $entries[0]['dn'];
+        $username_dn = $entries[0]['dn'];
 
-        return $this->ldap->setPassword($token_dn, $password);
+        return $this->ldap->setPassword($username_dn, $password);
     }
 
-    public function changeUserName($token, $token_new)
+    public function changeUserName($username, $username_new)
     {
         $opt = & $this->options;
-        $filter = sprintf('(%s=%s)', $opt['token_attr'], $token);
-        $sr = $this->ldap->search($opt['token_base'], $filter);
+        $filter = sprintf('(%s=%s)', $opt['ldap.username_attr'], $username);
+        $sr = $this->ldap->search($opt['ldap.username_base'], $filter);
         $entries = $this->ldap->entries($sr);
         if (!$entries['count']) return FALSE;
         $old_dn = $entries[0]['dn'];
         list($first,$rest) = explode(',', $old_dn, 2);
         list($k, $v) = explode('=', $first, 2);
 
-        return $this->ldap->rename($old_dn, $k.'='.$token_new) && $this->ldap->modReplace(
-            $k.'='.$token_new.','.$rest,
+        return $this->ldap->rename($old_dn, $k.'='.$username_new) && $this->ldap->modReplace(
+            $k.'='.$username_new.','.$rest,
             array(
-                $opt['token_attr'] => $token_new
+                $opt['ldap.username_attr'] => $username_new
             )
         );
     }
 
-    public function add($token, $password)
+    public function add($username, $password)
     {
         $opt = & $this->options;
 
-        return $this->ldap->addAccount($opt['token_base'], $token, $password);
+        return $this->ldap->addAccount($opt['ldap.username_base'], $username, $password);
     }
 
-    public function remove($token)
+    public function remove($username)
     {
         $opt = & $this->options;
-        $filter = sprintf('(%s=%s)', $opt['token_attr'], $token);
-        $sr = $this->ldap->search($opt['token_base'], $filter);
+        $filter = sprintf('(%s=%s)', $opt['ldap.username_attr'], $username);
+        $sr = $this->ldap->search($opt['ldap.username_base'], $filter);
         $entries = $this->ldap->entries($sr);
         if (!$entries['count']) return TRUE;
-        $token_dn = $entries[0]['dn'];
+        $username_dn = $entries[0]['dn'];
 
-        return $this->ldap->delete($token_dn);
+        return $this->ldap->delete($username_dn);
     }
 
 }
